2788a1a79ccb2baa6c13c4b181c2ed6870814282
Ivan de Oliveira <ivanoliven.js@gmail.com>
2023-07-17 13:17:10 -0300

    atualizado

---
+++ js/testeFinalModulo3.js
@@ -0,0 +1,70 @@
+// // // // let n = 10;
+// // // // let m = n + 50;
+
+// // // // console.log(m)
+// // // // console.log(n)
+// // // let nr = "1";
+// // // let x = (nr === 1);
+// // // let y = (nr == 1);
+// // // let z = (nr = 1);
+
+// // // console.log(x, y, z)
+
+// // let test = prompt("Hello", "World");
+// // // console.log(test)
+
+// let contacts = [{
+//     name: "Maxwell Wright",
+//     phone: "(0191) 719 6495",
+//     email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+// }, {
+//     name: "Raja Villarreal",
+//     phone: "0866 398 2895",
+//     email: "posuere.vulputate@sed.com"
+// }, {
+//     name: "Helen Richards",
+//     phone: "0800 1111",
+//     email: "libero@convallis.edu"
+// }];
+
+// contacts[3] = {
+//     name: "xx xx",
+//     phone: "99 99 ",
+//     email: "teste@teste.com"
+// }
+
+// for (let x = 0; x < contacts.length; x++) {
+//     console.log(contacts[x])
+// }
+
+// let testeVariavel = ['a', 'b', 'c', 'd', 'e'];
+
+// for (let x = testeVariavel.length - 1; x >= 0; x--) {
+
+//     console.log(testeVariavel[x])
+// }
+
+// for (let x = 10; x > 1; x -= 2)
+
+//     console.log("hello");
+
+// let animals = ["dog", "cat", "hamster", "rabbit"];
+
+// for (let i = 3; i < animals.length; i++)
+//     console.log(animals[i]);
+
+// let a = 5; while (a > 1)
+//     console.log(a--);
+
+// // for (let a = 5; a > 1; a--) {
+// //     console.log(a);
+// // }
+// // ;
+
+// for (let a = 4; a < 4; a++) {
+//     console.log("test");
+// }
+
+let car = { make: "Citroen", model: "DS" };
+for (let f in car)
+    console.log(f);
